b4c268371eb4f7b0859d70935685436b42bd920a
Connor <vanmeter.connor21@gmail.com>
2020-03-04 16:14:14 -0700

    Added initialization functionality. Began implementing evaluate_shot.

--- src/Server.cpp
+++ src/Server.cpp
@@ -18,6 +18,10 @@
 #include "common.hpp"
 #include "Server.hpp"
 
+#include "fstream"
+#include "iostream"
+using namespace std;
+
 
 /**
  * Calculate the length of a file (helper function)
@@ -38,10 +42,77 @@ int get_file_length(ifstream *file){
 void Server::initialize(unsigned int board_size,
                         string p1_setup_board,
                         string p2_setup_board){
+    /*The method opens player setup boards containing ship positions and checks the size of boards compared to
+     * board_size parameter.
+     */
+
+    //open setup_boards
+    std::ifstream p1_in("player_1.setup_board.txt");
+    char character1; int count1 = 0;
+    while (p1_in >> character1) ++count1;
+    std::cout << count1/10 << " characters in Player 1 board\n";
+
+    std::ifstream p2_in("player_2.setup_board.txt");
+    char character2; int count2 = 0;
+    while (p2_in >> character2) ++count2;
+    std::cout << count2/10 << " characters in Player 2 board\n";
+
+    std::cout << board_size << " characters in board size\n";
+
+    std::cout << p1_setup_board << " read in\n";
+    std::cout << p2_setup_board << " read in\n";
+
+    //do the player boards match?
+    if (count1 == count2/10 == board_size){
+        //Throw exception??
+        throw ServerException("Passed!");
+    }
+    if (count1/10 != count2/10 || count1/10 != board_size){
+        //Throw another exception
+        throw ServerException("Failed, board sizes not correct.");
+    }
+    if (p1_setup_board != "player_1.setup_board.txt" || p2_setup_board != "player_2.setup_board.txt"){
+        throw ServerException("File names are not valid. Please try again.");
+    }
+
 }
 
 
 int Server::evaluate_shot(unsigned int player, unsigned int x, unsigned int y) {
+    /*Checks the coordinates of a shot against setup board of player
+     *
+     * Check that player number within bounds, checks that shot coordinates within bounds, determines if the shot
+     * results in a HIT, or a MISS.
+     * @param player - player number
+     * @param x - coordinate
+     * @param y - coordinate
+     * @return returns shot result as either HIT, MISS, or OUT_OF_BOUNDS
+     */
+
+    //Check player number is within bounds
+    if (player < 1){
+        throw ServerException("Player number is low.");
+    }
+    if (player > 2){
+        throw ServerException("Player number is high.");
+    }
+
+    //Check that shot coordinates are within bounds, if so, return 0
+    if (x > BOARD_SIZE || x < 0){
+        cout << "X coordinates is not within bounds" << endl;
+        return 0;
+    }
+    if (y > BOARD_SIZE || y < 0){
+        cout << "Y coordinates is not within bounds" << endl;
+        return 0;
+    }
+
+    //Determine if shot is a miss, if so, return -1
+    //if ()
+
+    //Determine if shot is a hit, if so, return 1
+
+    return 3;
 }
 
 
