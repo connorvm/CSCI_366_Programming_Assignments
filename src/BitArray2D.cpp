f49a38f3d5a149ff3129db9cf38f86b7ef8490b1
Connor <vanmeter.connor21@gmail.com>
2020-04-10 12:50:34 -0600

    BitArray2D get array ready

--- src/BitArray2D.cpp
+++ src/BitArray2D.cpp
@@ -18,6 +18,40 @@
 #include "BitArray2D.hpp"
 
 BitArray2D::BitArray2D(unsigned int rows, unsigned int columns) {
+    /**
+    * Sets up the array to store rows * columns bits
+    * @param rows - number of rows
+    * @param columns - number of columns
+    */
+    char *array;
+    int num_bits;
+    int num_chars;
+    int mod_check;
+
+    cout << "---------------------------------------\n";
+    num_bits = rows * columns;
+    cout << "num_bits = " << num_bits << endl;
+    num_chars = num_bits/8;
+    cout << "num_chars = " << num_chars << endl;
+    mod_check = num_bits%8;
+    cout << "mod_check = " << mod_check << endl;
+
+    if (num_bits == 0){
+        throw BitArray2DException("0 Rows and 0 Columns. No array to make.");
+    } else if (mod_check == 0) {
+        //num_chars is the number needed
+        cout << "mod_check = 0\n";
+        cout << "num_chars = " << num_chars << endl;
+        *array = array[num_chars];
+        cout << "---------------------------------------\n";
+    } else if (mod_check > 0){
+        num_chars += 1; //need to add one more char
+        cout << "mod_check > 0\n";
+        cout << "num_chars = " << num_chars << endl;
+       *array = array[num_chars];
+        cout << "---------------------------------------\n";
+    }
+
 
 }
 
@@ -28,7 +62,14 @@ BitArray2D::~BitArray2D() {
 
 
 bool BitArray2D::get(unsigned int row, unsigned int column){
+    /**
+    * Get bit at row and column
+    * @param row
+    * @param column
+    * @return bit at row and column as bool
+    */
    // check array bounds
+   //int bounds = ;
 
    // get the element
    return get_bit_elem(array, columns, row, column);
